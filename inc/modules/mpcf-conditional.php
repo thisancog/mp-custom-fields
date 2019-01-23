85ae71d5b5a1ba25a15eebe6509f071ba4afe3e8
Matthias Planitzer <thisancog@gmail.com>
2019-01-23 16:10:43 +0100

    Upgraded conditional field to show checkbox if only options is available

--- inc/modules/mpcf-conditional.php
+++ inc/modules/mpcf-conditional.php
@@ -35,7 +35,9 @@ class MPCFConditionalField extends MPCFModule {
 	}
 
 	function build_field($args = array()) {
-		$value   = isset($args['value']) && !empty($args['value'][0]) ? unserialize($args['value'][0]) : array();
+		$value   = isset($args['value']) && !empty($args['value']) ? $args['value'] : array();
+		if (is_string($value))
+			$value = unserialize($value);
 
 		$label   = isset($args['label']) && !empty($args['label']) ? $args['label'] : '';
 		$options = isset($args['options']) && !empty($args['options']) ? $args['options'] : array();
@@ -49,7 +51,34 @@ class MPCFConditionalField extends MPCFModule {
 			<div class="mpcf-conditional-choice">
 <?php		if (!empty($label)) { ?>
 				<div class="mpcf-title"><label for="<?php echo $args['name']; ?>-type"><?php echo $label; ?></label></div>
-<?php 		} ?>
+<?php 		}
+
+			if (count($options) == 1) {
+				$name = key($options);
+				$params = $options[$name];
+
+				if (is_array($params['title']))	extract($params['title']);
+	 			else 							$title = $params['title'];
+
+	 			$disabled = false;
+
+
+
+	 			$selected = isset($value['type']) && $value['type'] == $name;
+	 			$disabled = isset($disabled) && $disabled ? ' disabled' : '';
+			 ?>
+
+			<input	type="checkbox"
+					name="<?php echo $args['name']; ?>[type]"
+					id="<?php echo $args['name']; ?>-type"
+					value="<?php echo $name; ?>"
+					<?php echo ($selected ? ' checked' : '') . $disabled; ?>
+					data-basename="<?php echo $args['name']; ?>"
+					data-options="<?php echo esc_attr(json_encode($options, JSON_HEX_QUOT | JSON_HEX_APOS)); ?>"
+					data-values="<?php echo esc_attr(json_encode($value, JSON_HEX_QUOT | JSON_HEX_APOS)); ?>">
+			<label for="<?php echo $args['name']; ?>-type"><?php echo $title; ?></label>
+
+<?php		} else { ?>
 
 				<select name="<?php echo $args['name']; ?>[type]"
 						id="<?php echo $args['name']; ?>-type"
@@ -70,6 +99,7 @@ class MPCFConditionalField extends MPCFModule {
 					<option value="<?php echo $name; ?>" <?php echo ($selected ? ' selected' : '') . $disabled; ?>><?php echo $title; ?></option>
 <?php			} ?>
 				</select>
+<?php 		} ?>
 			</div>
 
 			<div class="mpcf-conditional-wrapper"></div>
