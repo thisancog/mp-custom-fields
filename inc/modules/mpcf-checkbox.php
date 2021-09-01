1add4d26ae977829952839f0a0b469edbfcb7956
Matthias Planitzer <thisancog@gmail.com>
2021-09-01 18:33:39 +0200

    Add files via upload

--- inc/modules/mpcf-checkbox.php
+++ inc/modules/mpcf-checkbox.php
@@ -45,12 +45,13 @@ class MPCFCheckbox extends MPCFModule {
 	}
 
 	function build_field($args = array()) {
-		$label = isset($args['label']) && !empty($args['label']) ? $args['label'] : ''; ?>
+		$label = isset($args['label']) && !empty($args['label']) ? $args['label'] : '';
+		$value = $args['value'] === 'checked' || $args['value'] === '1'; ?>
 
 		<input  type="checkbox"
 				name="<?php echo $args['name']; ?>"
 				value="checked"
-				<?php echo mpcf_list_input_params($this, false) . ($args['value'] ? ' checked' : ''); ?>>
+				<?php echo mpcf_list_input_params($this, false) . ($value ? ' checked' : ''); ?>>
 			<label for="<?php echo $args['name']; ?>"><?php echo $args['label']; ?></label>
 <?php
 	}
