5d22633b44323813b48bd534438b5930ccc58b04
Matthias Planitzer <thisancog@gmail.com>
2021-09-02 15:16:31 +0200

    New field: grid

---
+++ inc/modules/mpcf-grid.php
@@ -0,0 +1,88 @@
+<?php
+
+if (!defined('ABSPATH')) exit;
+if (!class_exists('MPCFGridField')) :
+
+/*****************************************************
+	Grid field
+ *****************************************************/
+
+class MPCFGridField extends MPCFModule {
+	public $name = 'grid';
+
+	function __construct() {
+		parent::__construct();
+
+		// Supply a category for the field selection menu
+		// possible values: date, text, options, number, misc
+		// default: misc
+		$this->category = 'misc';
+
+		// If this field contains html5 input elements and therefore requires
+		// a browser compatibility check
+		$this->html5 = false;
+
+		// include additional classes for the wrapper of this field
+		$this->wrapperClasses = '';
+
+		// Parameters for the field which can be set by the user
+		// 'description' will be automatically added and ouput by the plugin
+		$this->parameters = array(
+			array(
+				'name'	=> 'columns',
+				'title' => __('Columns', 'mpcf'),
+				'type'	=> 'number',
+				'min'	=> 1
+			),
+			array(
+				'name'	=> 'rows',
+				'title' => __('Rows', 'mpcf'),
+				'type'	=> 'number',
+				'min'	=> 1
+			),
+			array(
+				'name'	=> 'required',
+				'title' => __('Required', 'mpcf'),
+				'type'	=> 'truefalse',
+				'default'	=> false
+			)
+		);
+	}
+
+	function label() {
+		return __('Grid field', 'mpcf');
+	}
+
+	function build_field($args = array()) {
+		$params = mpcf_list_input_params($this, array('required', 'min', 'max'));
+		$value  = json_decode(wp_specialchars_decode($args['value']), true);
+
+		if (empty($value))
+			$value = array('startrow' => 0, 'endrow' => 0, 'startcol' => 0, 'endcol' => 0);
+
+		$valueJSON = esc_attr(json_encode($value, JSON_HEX_QUOT | JSON_HEX_APOS | JSON_HEX_AMP)); ?>
+
+		<div class="grid-field-grid">
+<?php 		for ($row = 0; $row < $args['rows']; $row++) { ?>
+			<div class="row">
+<?php			for ($col = 0; $col < $args['cols']; $col++) {
+					$isSelected = $row >= $value['startrow'] && $row <= $value['endrow'] &&
+								  $col >= $value['startcol'] && $col <= $value['endcol']; ?>
+					<div class="grid-cell<?php echo $isSelected ? ' selected' : ''; ?>" data-row="<?php echo $row; ?>" data-col="<?php echo $col; ?>" draggable="false"></div>
+<?php			} ?>
+			</div>
+<?php 		} ?>
+		</div>
+
+		<input
+			type="hidden"
+			name="<?php echo $args['name']; ?>"
+			value="<?php echo $valueJSON; ?>"
+			class="grid-field-input"<?php echo $params; ?>>
+<?php 
+	}
+}
+
+endif;
+
+?>
