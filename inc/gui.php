37dc3d4a2e45e193baa0406f5668da8297d69fc4
Matthias Planitzer <14925402+thisancog@users.noreply.github.com>
2023-02-16 11:59:57 +0100

    Complete overhaul of loading of nested elements to get rid of dynamic loading where possible
    - separate activeTab IDs per panel set
    - creation of conditional panels on page request, keeping association to select field in- or outside repeater fields
    - Conditional Panels walker class to look for conditional panels, fields and values attached to them
    - stripping AJAX field getters for repeaters, conditionals and conditional panels of any logic to fill them with values, now only used to fetch new row/option field sets
    - tidy up mpcf_build_gui_from_fields, putting all value determination logic into separate function

--- inc/gui.php
+++ inc/gui.php
@@ -9,16 +9,14 @@ function mpcf_meta_box_init($post, $metabox) {
 	$boxes  = get_option('mpcf_meta_boxes', array());
 	$box    = $boxes[$metabox['id']];
 	$values = get_post_meta($post->ID, '', true);
-	wp_nonce_field('mpcf_meta_box_nonce', 'mpcf_meta_box_nonce'); ?>
-
+	$nonce  = 'mpcf_meta_box_nonce_' . $metabox['id'];
+	wp_nonce_field($nonce, $nonce); ?>
 	<div class="mpcf-parent">
-
 <?php 	if (isset($box['panels'])) {
-			mpcf_build_gui_as_panels($box['panels'], $values);
+			mpcf_build_gui_as_panels($metabox['id'], $box['panels'], $values);
 		} else if (isset($box['fields'])) {
 			mpcf_build_gui_from_fields($box['fields'], $values);
 		} ?>
-
 	</div>
 
 <?php 
@@ -51,7 +49,8 @@ function mpcf_build_admin_gui($panels, $optionName) {
 			}
 		}
 
-		$values['mpcf-activetab'] = isset($_POST['mpcf-activetab']) ? $_POST['mpcf-activetab'] : 0;
+		$tab = 'mpcf-activetab-' . $optionName;
+		$values[$tab] = isset($_POST[$tab]) ? $_POST[$tab] : 0;
 		update_option($optionName, $values);
 	}
 
@@ -71,7 +70,7 @@ function mpcf_build_admin_gui($panels, $optionName) {
 			<div id="message" class="mpcf-message updated fade"><p><strong><?php echo $message; ?></strong></p></div>
 <?php	}
 
-			mpcf_build_gui_as_panels($panels, $values); ?>
+			mpcf_build_gui_as_panels($optionName, $panels, $values); ?>
 
 			<div class="mpcf-options-inputs">
 				<input type="hidden" name="update_settings" id="update_settings" value="Y" />
@@ -91,26 +90,34 @@ function mpcf_build_admin_gui($panels, $optionName) {
 	Build graphical user interface component wise
  *****************************************************/
 
-function mpcf_build_gui_as_panels($panels, $values) { 
-	$activetab = isset($values['mpcf-activetab']) ? $values['mpcf-activetab'][0] : 0;
+function mpcf_build_gui_as_panels($id, $panels, $values) {
+	$updated = mpcf_add_conditional_panels($panels, $values);
+	$panels = $updated['panels'];
+	$values = $updated['values'];
+
+	$panels = array_map(function($panel) {
+		$panel['panel_id']       = isset($panel['panel_id'])       ? $panel['panel_id']       : uniqid();
+		$panel['panel_basename'] = isset($panel['panel_basename']) ? $panel['panel_basename'] : '';
+		return $panel;
+	}, $panels);
+
+	$tab        = 'mpcf-activetab-' . $id;
+	$activetab  = isset($values[$tab]) ? $values[$tab][0] : 0;
 	$hasEditors = false; ?>
 
 	<div class="mpcf-panels">
-		<input type="hidden" name="mpcf-activetab" class="activetab" value="<?php echo $activetab; ?>" />
+		<input type="hidden" name="<?php echo $tab; ?>" class="activetab" value="<?php echo $activetab; ?>" />
 		<ul class="mpcf-panels-menu">
-
 <?php	for ($i = 0; $i < count($panels); $i++) {
 			mpcf_build_panel_menu_item($panels[$i], $i);
 		} ?>
-
 		</ul>
 
 		<div class="mpcf-panels-tabs">
-
 <?php	for ($i = 0; $i < count($panels); $i++) {
-			$hasEditors = $hasEditors || mpcf_build_panel_tab($panels[$i], $values, $i);
+			$hasThisTabEditors = mpcf_build_panel_tab($panels[$i], $values, $i);
+			$hasEditors        = $hasEditors || $hasThisTabEditors;
 		} ?>
-
 		</div>
 
 	</div>
@@ -124,32 +131,33 @@ function mpcf_build_gui_as_panels($panels, $values) {
 		if ($screen->parent_base !== 'edit') { ?>
 			<div class="mpcf-editor-instance"><?php wp_editor('', 'mpcf-editor-instance'); ?></div>
 <?php	}
-	} ?>
-
-<?php	
-
+	}
 }
 
 
-function mpcf_build_panel_menu_item($panel, $i) { ?>
-	<li class="mpcf-panel-item" data-index="<?php echo $i; ?>">
 
+
+function mpcf_build_panel_menu_item($panel, $i) { ?>
+	<li class="mpcf-panel-item" data-index="<?php echo $i; ?>" data-panel-id="<?php echo $panel['panel_id']; ?>" data-basename="<?php echo $panel['panel_basename']; ?>">
 <?php 	if (isset($panel['icon'])) {
 			if (strpos($panel['icon'], 'dashicons') > -1) { ?>
-				<div class="mpcf-panel-icon dashicons <?php echo $panel['icon']; ?>"></div>
+			<div class="mpcf-panel-icon dashicons <?php echo $panel['icon']; ?>"></div>
 <?php 		} else { ?>
-				<div class="mpcf-panel-icon mpcf-panel-icon-svg" style="background-image: url(<?php echo $panel['icon']; ?>);"></div>
+			<div class="mpcf-panel-icon mpcf-panel-icon-svg" style="background-image: url(<?php echo $panel['icon']; ?>);"></div>
 <?php 		}
-		} ?>
+		}
 
-			<span class="mpcf-panel-title"><?php echo $panel['name']; ?></span>
+		$title = isset($panel['name'])  ? $panel['name']  : '';
+		$title = isset($panel['title']) ? $panel['title'] : $title; ?>
+			<span class="mpcf-panel-title"><?php echo $title; ?></span>
 	</li>
 <?php
 }
 
 
-function mpcf_build_panel_tab($panel, $values, $i) { ?>
-	<div class="mpcf-panel" data-index="<?php echo $i; ?>">
+function mpcf_build_panel_tab($panel, $values, $i) {
+	$className = 'mpcf-panel' . (isset($panel['class_name']) ? ' ' . $panel['class_name'] : ''); ?>
+	<div class="<?php echo $className; ?>" data-index="<?php echo $i; ?>" data-panel-id="<?php echo $panel['panel_id']; ?>" data-basename="<?php echo $panel['panel_basename']; ?>">
 <?php 	mpcf_build_gui_from_fields($panel['fields'], $values);
 		$hasEditors = mpcf_ajax_enqueue_editors($panel['fields']); ?>
 	</div>
@@ -169,51 +177,38 @@ function mpcf_build_gui_from_fields($fields, $values, $echoRequired = true) {
 	foreach ($fields as $field) {
 		if (!isset($field['type'])) continue;
 
-		$type = $field['type'];
-		$field = mpcf_sanitize_args($field);
-		$actions = isset($field['actions']) ? $field['actions'] : array();
+		$type             = $field['type'];
+		$field            = mpcf_sanitize_args($field);
 
 		$field['post_id'] = $id;
-		$field['value'] = isset($field['value']) ? $field['value'] : null;
+		$field['value']   = mpcf_get_field_value($field, $values);
+		$field            = mpcf_resolve_deep_fields($field);
 
-		if ($field['value'] === null || empty($field['value'])) {
-			$field['value'] = isset($values[$field['name']]) ? $values[$field['name']] : $field['default'];
-		}
-
-		$field['value'] = mpcf_resolve_sanitized_fields($field['value']);
-
-		if (isset($actions['display_before'])) {
-			$field['value'] = call_user_func($actions['display_before'], $id, $field['name'], $field['value']);
-		}
-
-		$field = mpcf_resolve_deep_fields($field);
-
-		$required = !$required && $field['required'] ? true : $required;
-		$hasRequireds = false;
+		$required         = !$required && $field['required'] ? true : $required;
+		$hasRequireds     = false;
 
 		if (isset($o['modules'][$type])) {
 			$classname = $o['modules'][$type]['name'];
-			$module = new $classname();
+			$module    = new $classname();
 
-			$isRequired = isset($field['required']) && $field['required'] ? ' mpcf-required' : '';
-			$hasHTML5   = isset($module->html5) && $module->html5 ? ' mpcf-nohtml5' : '';
-			$html5Test  = isset($module->html5) && $module->html5 ? ' data-invalid-test="Not-a-valid-value"' : '';
+			$classes  = isset($field['required']) && $field['required'] ? ' mpcf-required' : '';
+			$classes .= isset($module->html5) && $module->html5 ? ' mpcf-nohtml5' : '';
+			$attrs    = isset($module->html5) && $module->html5 ? ' data-invalid-test="Not-a-valid-value"' : '';
+
+			if ($type == 'conditionalpanels' && isset($field['value'][$field['name']]['panel_id'])) {
+				$attrs  .= ' data-panel-id="' . $field['value'][$field['name']]['panel_id'] . '"';
+				unset($field['value'][$field['name']]['panel_id']);
+			}
+
 			$wrapperClasses = isset($module->wrapperClasses) && !empty($module->wrapperClasses) ? ' ' . $module->wrapperClasses : ''; ?>
 
-			<div class="mpcf-<?php echo $type; ?>-input mpcf-field-option<?php echo $hasHTML5 . $isRequired; ?>"id="mpcf-field-<?php echo $field['name']; ?>" <?php echo $html5Test; ?>>
-
-<?php		if (isset($field['title']) && !empty($field['title'])) { ?>
-				<div class="mpcf-title"><label for="<?php echo $field['name']; ?>"><?php echo $field['title']; ?></label></div>
-<?php 		} ?>
-						
+			<div class="mpcf-<?php echo $type; ?>-input mpcf-field-option<?php echo $classes; ?>" id="mpcf-field-<?php echo $field['name']; ?>"<?php echo $attrs; ?>>
+				<?php mpcf_insert_field_title($field); ?>
 				<div class="mpcf-field<?php echo $wrapperClasses; ?>">
 <?php				$module->args = $field;
 					$result = $module->build_field($field);
 					$hasRequireds = $hasRequireds || $result;
-
-					if (!empty($field['description']) && $field['description'] !== false) { ?>
-						<div class="mpcf-description"><?php echo $field['description']; ?></div>
-<?php 				} ?>
+					mpcf_insert_field_description($field); ?>
 				</div>
 			</div>
 <?php	}
@@ -226,6 +221,59 @@ function mpcf_build_gui_from_fields($fields, $values, $echoRequired = true) {
 		mpcf_required_hint();
 	}
 }
+
+
+
+
+function mpcf_get_field_value($field, $values) {
+	$value = isset($field['value']) ? $field['value'] : null;
+
+	if ($value === null || empty($value)) {
+		$value = isset($values[$field['name']]) ? $values[$field['name']] : $field['default'];
+		$value = mpcf_resolve_sanitized_fields($value);
+	}
+
+	if ($field['type'] == 'conditionalpanels') {
+		$value = $values;
+	}
+
+	if (isset($field['conditional_extracted_value'])) {
+		$value = $field['conditional_extracted_value'];
+		$value = mpcf_resolve_sanitized_fields($value);
+		$value = isset($value[$field['name']]) ? $value[$field['name']] : $field['default'];
+		if  ($field['type'] == 'repeater' &&
+			(is_array($value) && array_keys($value) !== range(0, count($value) - 1))) {
+			$value = array($value);
+		}
+	}
+
+//	repeater values must be sequential arrays, each value for one row
+	if ($field['type'] == 'repeater') {
+		if (is_array($value) && array_keys($value) !== range(0, count($value) - 1)) {
+			$value = array($value);
+		}
+	}
+
+	if (isset($field['actions']) && isset($field['actions']['display_before'])) {
+		$value = call_user_func($field['actions']['display_before'], $id, $field['name'], $value);
+	}
+
+	return $value;
+}
+
+function mpcf_insert_field_title($field) {
+	if (isset($field['title']) && !empty($field['title'])) { ?>
+<div class="mpcf-title"><label for="<?php echo $field['name']; ?>"><?php echo $field['title']; ?></label></div>
+<?php 
+	}
+}
+
+function mpcf_insert_field_description($field) {
+	if (!empty($field['description']) && $field['description'] !== false) { ?>
+<div class="mpcf-description"><?php echo $field['description']; ?></div>
+<?php 	}
+}
+
 
 
 function mpcf_resolve_sanitized_fields($var) {
@@ -241,7 +289,6 @@ function mpcf_resolve_sanitized_fields($var) {
 
 	return $var;
 }
-
 
 function mpcf_resolve_deep_fields($field) {
 	$type = $field['type'];
@@ -287,6 +334,188 @@ function mpcf_get_queried_object_id() {
 
 
 
+
+/*****************************************************
+	Add conditional panels
+ *****************************************************/
+
+function mpcf_add_conditional_panels($panels, $values) {
+	$adder = new MPCFRecursiveConditionalPanelsAdder($panels, $values);
+	$adder->start_search();
+
+	return [ 'panels' => $adder->get_merged_panels(), 'values' => $adder->get_merged_values() ];
+}
+
+Class MPCFRecursiveConditionalPanelsAdder {
+	public $allPanels;
+	public $allValues;
+	public $conditionalPanels = array();
+
+	function __construct($allPanels, $allValues = array()) {
+		$this->allPanels = $allPanels;
+		$this->allValues = $allValues;
+	}
+
+	public function start_search() {
+		$this->add_layer($this->allPanels, null, [ 'path' => [], 'name' => [] ]);
+	}
+
+	public function add_layer($layer, $key, $args = []) {
+		if (!is_array($layer)) return;
+
+		if ($key !== null)
+			$args['path'][] = $key;
+
+		if (isset($layer['name']))
+			$args['name'][] = $layer['name'];
+
+		$baseName = $this->get_field_base_name($args);
+		$baseNameConcat = implode('', array_map(function($i) use ($baseName) {
+			return $i == 0 ? $baseName[$i] : '[' . $baseName[$i] . ']';
+		}, array_keys($baseName)));
+
+		if (isset($layer['type']) && $layer['type'] == 'conditionalpanels') {
+			$value = $this->get_value($baseName);
+			if ($value == null) return;
+
+			if (!isset($value['type']))
+				return array_walk($layer, array($this, 'add_layer'), $args);
+
+			$type = $value['type'];
+
+			if (!isset($layer['options'][$type])) return;
+			$id = uniqid();
+
+			$chosenOption                   = $layer['options'][$type]['panel'];
+			$chosenOption['class_name']     = 'mpcf-conditionalpanel';
+			$chosenOption['panel_id']       = $id;
+			$chosenOption['panel_basename'] = count($args['name']) > 1 ? $args['name'][1] : $args['name'][0];
+
+			$chosenOption['fields']         = array_map(function($field) use ($value, $baseNameConcat, $chosenOption) {
+				if (!is_array($field)) return $field;
+
+				$field['conditional_extracted_value'] = $value;				
+				$field['baseName'] = $baseNameConcat;
+				return $field;
+			}, $chosenOption['fields']);
+
+			$chosenOption['fields'] = array_values(array_filter($chosenOption['fields']));
+
+			$this->add_panel_id_to_field($baseName, $id);			
+			$this->conditionalPanels[] = $chosenOption;
+		} else if (isset($layer['type']) && $layer['type'] == 'repeater') {
+			$repeaterValues = $this->get_value($baseName);
+			$repeaterValues = !is_array($repeaterValues) ? [ $repeaterValues ] : $repeaterValues;		
+
+			foreach ($repeaterValues as $index => $row) {
+				$newArgs = $args;
+				$newArgs['path'][] = $index;
+				$newArgs['name'][] = $index;
+				array_walk($layer['fields'], array($this, 'add_layer'), $newArgs);
+			}
+
+			return;
+		}
+
+		array_walk($layer, array($this, 'add_layer'), $args);
+	}
+
+	public function get_field_base_name($args) {
+		$baseName = [];
+
+		$value     = $this->allValues;
+		$fields    = $this->allPanels;
+		$firstName = true;
+		$fieldNames = $args['name'];
+
+		while (count($fieldNames) > 0) {
+			$name = array_shift($fieldNames);
+			if (!isset($value[$name])) {
+				if ($firstName) {
+					$firstName = false;
+					continue;
+				} else {
+					$baseName[] = $name;
+					break;
+				}
+			}
+
+			$baseName[] = $name;
+			$value = $value[$name];
+
+			if (is_array($value) && count($value) == 1 && isset($value[0]) && is_serialized($value[0])) {
+				$value = unserialize($value[0]);
+			}
+		}
+
+		return $baseName;
+	}
+
+
+	public function get_value($baseName) {
+		if (empty($baseName)) return array();
+		
+	//	apply field tree to values, could be handled in above while loop as well
+
+		$value = $this->allValues;
+		$name = $baseName[0];
+		
+		while (count($baseName) > 0) {
+			$name = array_shift($baseName);
+
+			if (!isset($value[$name]))
+				return $value;
+
+			$value = $value[$name];
+
+			if (is_array($value) && count($value) == 1 && isset($value[0]) && is_serialized($value[0])) {
+				$value = unserialize($value[0]);
+			}
+		}
+
+		return $value;
+	}
+
+	public function add_panel_id_to_field($baseName, $id) {
+		$baseNameCopy = $baseName;
+
+		$pointer      = &$this->allValues;
+		$name         = $baseNameCopy[0];
+		
+		while (count($baseNameCopy) > 0) {
+			$name = array_shift($baseNameCopy);
+
+			if (!isset($pointer[$name]))
+				break;
+
+			$pointer = &$pointer[$name];
+
+			if (is_array($pointer) && count($pointer) == 1 && isset($pointer[0]) && is_serialized($pointer[0])) {
+				$pointer = unserialize($pointer[0]);
+				$pointer = &$pointer;
+			}
+		}
+
+		if (is_array($pointer))
+			$pointer['panel_id'] = $id;
+		unset($pointer);
+
+		$name = $baseName[0];
+		$this->allValues[$name] = [ serialize($this->allValues[$name]) ];
+	}
+
+	public function get_merged_panels() {
+		return array_merge($this->allPanels, $this->conditionalPanels);
+	}
+
+	public function get_merged_values() {
+		return $this->allValues;
+	}
+}
+
+
+
+
 /*****************************************************
 	Sanitize fields parameters
  *****************************************************/
@@ -310,13 +539,15 @@ function mpcf_sanitize_args($args) {
 
 function mpcf_save_meta_boxes($post_id) {
 	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
-	if (!isset($_POST['mpcf_meta_box_nonce']) || !wp_verify_nonce($_POST['mpcf_meta_box_nonce'], 'mpcf_meta_box_nonce')) return;
 	if (!current_user_can('edit_post', $post_id)) return;
 
 	$post_type = get_post_type($post_id);
-	$boxes = mpcf_get_metaboxes_for_type($post_type);
+	$boxes     = mpcf_get_metaboxes_for_type($post_type);
 
-	foreach ($boxes as $box) {
+	foreach ($boxes as $id => $box) {
+		$nonce = 'mpcf_meta_box_nonce_' . $id;
+		if (!isset($_POST[$nonce]) || !wp_verify_nonce($_POST[$nonce], $nonce)) return;
+
 		$fields = array();
 		if (isset($box['panels'])) {
 			array_walk($box['panels'], function($panel) use (&$fields) {
@@ -336,11 +567,11 @@ function mpcf_save_meta_boxes($post_id) {
 		}
 	}
 
-
-	if (isset($_POST['mpcf-activetab']))	
-		update_post_meta($post_id, 'mpcf-activetab', $_POST['mpcf-activetab']);
+	array_walk($_POST, function($value, $key) use ($post_id) {
+		if (strpos($key, 'mpcf-activetab') === false) return;
+		update_post_meta($post_id, $key, $value);
+	});
 }
-
 
 
 
@@ -349,35 +580,15 @@ function mpcf_save_meta_boxes($post_id) {
  *****************************************************/
 
 function mpcf_ajax_get_repeater_row() {
-	global $post;
-
-	$post = get_post($_POST['id']);
-
 	$fields = json_decode(stripcslashes($_POST['fields']), true);
-
-	$values = array();
 	$buttons = '<div class="mpcf-repeater-row-controls"><div class="mpcf-repeater-row-remove dashicons-before dashicons-trash"></div><div class="mpcf-repeater-row-move dashicons-before dashicons-move"></div></div>';
 
 	$enqueueEditor = false;
 
 	ob_start();
-	if (isset($_POST['values'])) {
-		$values = json_decode(stripcslashes($_POST['values']), true);
-
-		if ($values !== false && !empty($values) && !(count($values) === 1 && empty($values[0]))) {
-			foreach ($values as $i => $row) { ?>
-				<li class="mpcf-repeater-row">
-<?php 				mpcf_build_gui_from_fields($fields, $row, false);
-					$enqueueEditor = $enqueueEditor || mpcf_ajax_enqueue_editors($fields);
-					echo $buttons; ?>
-				</li>
-<?php		}
-		}
-	} else {
-		mpcf_build_gui_from_fields($fields, $values, false);
-		$enqueueEditor = $enqueueEditor || mpcf_ajax_enqueue_editors($fields);
-		echo $buttons;
-	}
+	mpcf_build_gui_from_fields($fields, array(), false);
+	$enqueueEditor = $enqueueEditor || mpcf_ajax_enqueue_editors($fields);
+	echo $buttons;
 
 	if ($enqueueEditor) {
 		\_WP_Editors::enqueue_scripts();
@@ -397,7 +608,9 @@ function mpcf_ajax_enqueue_editors($fields) {
 	if (isset($fields[0]['fields']))
 		$hasEditors = $hasEditors || mpcf_ajax_enqueue_editors($fields[0]['fields']);
 
-	return $hasEditors || count(array_filter($fields, function($field) { return $field['type'] === 'editor'; })) > 0;
+	return $hasEditors || count(array_filter($fields, function($field) {
+		return $field['type'] === 'editor';
+	})) > 0;
 }
 
 
@@ -432,6 +645,9 @@ function mpcf_ajax_get_conditional_panels_fields() {
 	$panel  = json_decode(stripcslashes($_POST['panel']), true);
 	$values = array();
 
+	$panel['panel_id']       = isset($panel['panel_id'])       ? $panel['panel_id']       : uniqid();
+	$panel['panel_basename'] = isset($panel['panel_basename']) ? $panel['panel_basename'] : '';
+
 	$tab    = '';
 	$menu   = '';
 
@@ -445,7 +661,7 @@ function mpcf_ajax_get_conditional_panels_fields() {
 	ob_end_clean();
 
 	ob_start();
-	mpcf_build_panel_tab($panel, $values, false);
+	mpcf_build_panel_tab($panel, $values, -1);
 	$tab = ob_get_contents();
 	ob_end_clean();
 
